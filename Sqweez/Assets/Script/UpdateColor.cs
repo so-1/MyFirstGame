5e0c852eb08bf17747970998351507e9cb2f1c5f
so-1 <24763955+so-1@users.noreply.github.com>
2018-01-04 19:25:49 +0900

    UpdateColorスクリプト追加
    UpdateColorスクリプトを追加。その他いらないスクリプトを消したりした。(Win)

---
+++ Sqweez/Assets/Script/UpdateColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateColor : MonoBehaviour
+{
+    private GetColor getColor;
+
+    void Start()
+    {
+        getColor = GameObject.FindObjectOfType<GetColor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = getColor.color;
+    }
+}
